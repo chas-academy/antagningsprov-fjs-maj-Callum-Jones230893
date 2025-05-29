9fbc08e4d93c87d230d566aa544d335d7159094c
Callum Jones <call_jones2@hotmail.com>
2025-05-29 17:24:32 +0200

    Update uppg5.js

--- uppg5.js
+++ uppg5.js
@@ -3,8 +3,15 @@ function uppg5() {
     // skriv en if-sats som jämför två tal
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
     // annars skriv ut "Det andra talet är större än det första"
+    let num1= 5;
+    let num2= 17;
 
-
+    if (num1 > num2){
+        console.log("The first number is greater than the second")
+    }
+    else if (num1 < num2){
+        console.log("The second number is greater than the first")
+    }
 
 }
 
