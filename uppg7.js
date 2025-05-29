a183f38962b10e44ea71ecda1c5495d0c2973da2
Callum Jones <call_jones2@hotmail.com>
2025-05-29 17:35:50 +0200

    Update uppg7.js

--- uppg7.js
+++ uppg7.js
@@ -5,7 +5,11 @@ function uppg7() {
     // skapa en funktion som tar in två tal som argument
     // funktionen ska returnera summan av talen
     // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
+    function tal(number1, number2)  {
+      return number1 + number2;
+    }
 
+   return tal(5, 10);
  
 }
 
